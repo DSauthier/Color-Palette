42bb52335374eb923a48b2da4f00df7a913506ec
DSauthier <danielsauthier@live.com>
2020-05-17 11:25:17 -0400

    first style of colorbox

--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,21 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
+import ColorBox from './ColorBox'
+import './Palette.css'
 
 class Palette extends Component {
   render(){
+    const ColorBoxes = this.props.colors.map(color =>(
+      <ColorBox background={color.color} name={color.name}/>
+    ))
+
+
     return (
     <div className="Palette">
      {/* navbarGoesHere */}
 
 
       <div className="Palette-colors">
-        
+        {ColorBoxes}
         
       
       </div>      
